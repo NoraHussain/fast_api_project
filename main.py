58f46e394d653905b169b06bf10f551d12b56569
Nora Hussain <nora.hussain.1999@gmail.com>
2026-03-02 16:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# from fastapi import FastAPI
-# from database import Base, engine
-
-# import models.user
-# import models.project
-# import models.task
-
-# app = FastAPI()
-
-# Base.metadata.create_all(bind=engine)
-
-# @app.get("/")
-# def root():
-#     return {"message": "Hello FastAPI + SQLAlchemy!"}
-
 from fastapi import FastAPI
 
 from routers import users, projects, tasks
